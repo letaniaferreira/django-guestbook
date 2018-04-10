926d232b67571af5ae6492c989d9ea8f72d99793
Letania Ferreira <letaniaferreira@gmail.com>
2018-04-09 17:41:50 -0700

    added str function to models.py

--- myproject/guestbook/models.py
+++ myproject/guestbook/models.py
@@ -6,3 +6,5 @@ class Comment(models.Model):
     comment = models.TextField()
     date_added = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return '<Name: {}, ID: {}>'.format(self.name, self.id)
